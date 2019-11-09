936a8082d27c7421c0545d651f6ef73167838f88
OscarAkaElvis <oscar.alfonso.diaz@gmail.com>
2019-11-09 10:26:07 +0100

    Add plugin file. Not done yet

---
+++ realtek_chipset_fixer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#Global shellcheck disabled warnings
+#shellcheck disable=SC2034
+
+plugin_name="Realtek chipset fixer"
+plugin_description="A plugin to fix some problematic Realtek chipsets"
+plugin_author="OscarAkaElvis"
+
+plugin_enabled=1
+
+plugin_minimum_ag_affected_version="10.0"
+plugin_maximum_ag_affected_version=""
+plugin_distros_supported=("*")
+
+#TODO pending of plugin creation
